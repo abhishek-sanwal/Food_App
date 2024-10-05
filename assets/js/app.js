54c4e00bd7c0f088f97d4003d7490c719e906987
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-10-05 17:43:27 +0530

    :sparkles::sparkles: added dynamic copyright year

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,10 @@
 console.log("Js is running");
 
+// Function to implement smooth scrolling
 function addScroll(event) {
   console.log("Scroll", event);
+
+  // Prevent default behavior
   event.preventDefault();
 
   const href = this.getAttribute("href");
@@ -28,5 +31,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const headerEle = document.querySelector(".header");
   const links = document.querySelectorAll("a:link");
 
+  // Add copyright year in html dynamically.
+  const copyrightYear = document.querySelector(".copyright-year");
+  copyrightYear.textContent = new Date().getFullYear();
+
+  // Add smooth scrolling to each link
   links.forEach((link) => link.addEventListener("click", addScroll));
 });
