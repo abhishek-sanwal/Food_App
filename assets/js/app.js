27fdd2b5ddffc1cbb46829e43c808bce23b70b85
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-10-05 17:34:57 +0530

    :sparkles::sparkles: Added smooth scroll feature

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,32 @@
 console.log("Js is running");
+
+function addScroll(event) {
+  console.log("Scroll", event);
+  event.preventDefault();
+
+  const href = this.getAttribute("href");
+
+  if (href === "#") {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
+  if (href !== "#" && href.startsWith("#")) {
+    const node = document.querySelector(href);
+    node.scrollIntoView({ behavior: "smooth" });
+  }
+
+  // Remove mobile navbar
+  if (this.classList.contains("main-nav-list")) {
+    headerEle.classList.toggle("nav-open");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const headerEle = document.querySelector(".header");
+  const links = document.querySelectorAll("a:link");
+
+  links.forEach((link) => link.addEventListener("click", addScroll));
+});
